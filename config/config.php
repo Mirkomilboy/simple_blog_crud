68a2afd07b741063109e859fc14d828d643a1875
Mirkomilboy <mirkomilboysadirov1997@mail.ru>
2020-11-12 15:41:27 +0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-    define('ROOT_URL', 'http://localhost/phpsandbox/8_php_blog_crud/');
+    define('ROOT_URL', 'http://localhost/simple_blog_crud/');
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', '');
